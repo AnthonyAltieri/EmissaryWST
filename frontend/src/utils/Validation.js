f4e3d419dbbcdc46cb9f8de0ca22f0d057ece8aa
Daniel Wong <daw023@ucsd.edu>
2017-06-15 00:08:01 -0700

    utils

--- frontend/src/utils/Validation.js
+++ frontend/src/utils/Validation.js
@@ -2,6 +2,9 @@
  * @author Anthony Altieri on 6/10/17.
  */
 
+ /**
+ * Prunes phone numbers that are in the format (XXX)-XXX-XXXX 
+ */
 export let validatePhoneNumber = phoneNumber => {
   let pruned = phoneNumber
     .toString()
